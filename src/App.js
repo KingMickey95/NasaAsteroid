ddf6ff2642bab41ebaa44f003985f6e07c3cef73
Crystal Lo <crystallo@Crystals-MBP-2.attlocal.net>
2022-03-02 19:15:55 -0800

    wip

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <Space></Space>
+      <Space />
     </div>
   );
 }
--- src/Space.js
+++ src/Space.js
@@ -9,52 +9,64 @@ class Space extends React.Component {
     };
   }
 
-componentDidMount(){
-  this.fetchRock();
-}
-fetchRock(){
-  const API_KEY = 'fwlvjihpE1XYyI7gG2eFtBYytWa8gUPSqOjdSh4g';
-  let start_date = '2022-02-27';
-  let end_date = '2022-03-01';
-  let api_call = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start_date}&end_date=${end_date}&api_key=${API_KEY}`;
-  let listDate = [];
-  fetch(api_call)
+  componentDidMount(){
+    this.fetchRock();
+  }
+
+  fetchRock(){
+    const API_KEY = 'fwlvjihpE1XYyI7gG2eFtBYytWa8gUPSqOjdSh4g';
+    const start_date = '2022-02-27';
+    const end_date = '2022-03-01';
+    const api_call = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start_date}&end_date=${end_date}&api_key=${API_KEY}`;
+    const listDate = [];
+    
+    fetch(api_call)
       .then((res) => res.json())
-        
       .then((data) => {
-        console.log(data);
-       for(const date in data.near_earth_objects){
-
-        const newList = { ...this.state.asteroidList, date: data.near_earth_objects[date] };
-
-        this.setState( { asteroidList : newList } )
-
-        console.log("this is the date " + date);
-  
-        listDate.push(date);
-        
-       }
-
-       console.log(this.state.asteroidList);
-       console.log("this is it " + listDate);
-       
+        for(const date in data.near_earth_objects){
+          const newList = { ...this.state.asteroidList, date: data.near_earth_objects[date] };
+          this.setState( { asteroidList : newList } )
+    
+          listDate.push(date);
         }
-      )
-}
-  render() {
-    
-    
-    const asteroidInfo = this.state.asteroidList.forEach( (list) =>{
-      (list.map((asteroid) => 
-        
-        <tr index = {asteroid.name}>
-          <td>{asteroid.name}</td>
-        </tr>
-        )
-      )
-    });
+      });
+  }
 
-    
+  generateTable = () => {
+
+    const { asteroidList } = this.state;
+    const asteroidInfo = [];
+
+    for (const key in asteroidList) {
+      console.log("before: ", asteroidInfo)
+      for (const asteroid in asteroidList[key]) {
+        asteroidInfo.push((
+          <tr index={asteroid.name} key={asteroid.name}>
+           <td>{asteroid.name}</td>
+          </tr>)
+        )
+      }
+      console.log(asteroidList[key]);
+      console.log("after: ", asteroidInfo)
+    }
+
+    return asteroidInfo;
+  }
+
+  render() {
+
+    //const asteroidInfo = this.generateTable();
+
+    const { asteroidList } = this.state;
+    const asteroidInfo = Object.entries(asteroidList).map(([key, value]) => {
+      return value.map((asteroid) => {
+        return (<tr index={asteroid.name} key={asteroid.name}>
+          <td>{asteroid.name}</td>
+         </tr>)
+      })
+    })
+
+    console.log(asteroidInfo)
 
     return (
       
@@ -69,11 +81,9 @@ fetchRock(){
              {asteroidInfo}
            </tbody>
         </table> 
-          
-       
-
       </div>
     )
   }
 }
+
 export default Space;
